6672dcfffb8a13a1b9bae39f9d5ae80627f91ed4
Kevin Luo <ldkevin23307130031@gmail.com>
2026-06-07 16:13:43 +0800

    实现 VGG Batchnorm

--- models/vgg.py
+++ models/vgg.py
@@ -1,8 +1,12 @@
 """
 VGG
 """
+import sys
+import os
 import numpy as np
 from torch import nn
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from utils.nn import init_weights_
 
@@ -54,6 +58,75 @@ class VGG_A(nn.Module):
             nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1),
             nn.ReLU(True),
             nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1),
+            nn.ReLU(True),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * 1 * 1, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, num_classes))
+
+        if init_weights:
+            self._init_weights()
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x.view(-1, 512 * 1 * 1))
+        return x
+
+    def _init_weights(self):
+        for m in self.modules():
+            init_weights_(m)
+
+class VGG_A_BatchNorm(nn.Module):
+    """VGG_A model with Batch Normalization
+
+    Size of Linear layers is smaller since input assumed to be 32x32x3, instead of
+    224x224x3. Batch Normalization layers are added after each Conv2d and before ReLU.
+    """
+
+    def __init__(self, inp_ch=3, num_classes=10, init_weights=True):
+        super().__init__()
+
+        self.features = nn.Sequential(
+            # stage 1
+            nn.Conv2d(in_channels=inp_ch, out_channels=64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),  # 新增 BN 层
+            nn.ReLU(True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            # stage 2
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128), # 新增 BN 层
+            nn.ReLU(True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            # stage 3
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256), # 新增 BN 层
+            nn.ReLU(True),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256), # 新增 BN 层
+            nn.ReLU(True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            # stage 4
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512), # 新增 BN 层
+            nn.ReLU(True),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512), # 新增 BN 层
+            nn.ReLU(True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            # stage 5
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512), # 新增 BN 层
+            nn.ReLU(True),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512), # 新增 BN 层
             nn.ReLU(True),
             nn.MaxPool2d(kernel_size=2, stride=2))
 
